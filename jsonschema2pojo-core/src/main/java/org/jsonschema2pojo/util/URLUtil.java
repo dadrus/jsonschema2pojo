8d3126af074511cfff8abfd54ee582612a3464da
Dimitrij Drus <dadrus@gmx.de>
2016-01-12 08:28:05 +0100

    made URLUtils final with private constructir(it is a utility class). Added functions to resolve URLs from resource locations given as string and from base uris with fragments

--- jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/util/URLUtil.java
+++ jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/util/URLUtil.java
@@ -16,16 +16,20 @@
 
 package org.jsonschema2pojo.util;
 
-import org.apache.commons.lang.StringUtils;
-import org.jsonschema2pojo.URLProtocol;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-public class URLUtil {
+import org.apache.commons.lang.StringUtils;
+import org.jsonschema2pojo.URLProtocol;
+
+public final class URLUtil {
+
+    private URLUtil() {
+    }
 
     public static URLProtocol parseProtocol(String input) {
         return URLProtocol.fromString(StringUtils.substringBefore(input, ":"));
@@ -50,5 +54,70 @@ public class URLUtil {
         } catch (URISyntaxException e) {
             throw new IllegalArgumentException(String.format("URL contains an invalid URI syntax: %s", url), e);
         }
+    }
+
+    /**
+     * Resolve the given resource location to a {@link java.net.URL}.
+     * <p>
+     * This function simple returns the URL that the given
+     * {@code resourceLocation} would correspond.
+     * 
+     * <p>
+     * The resource location can be one of:
+     * <li>a "classpath:" pseudo URL,</li>
+     * <li>a "java:" pseudo URL,</li>
+     * <li>a "resource:" pseudo URL,</li>
+     * <li>a "file:" URL,</li>
+     * <li>a web ("http:, etc") URL,</li>
+     * <li>or a plain file path</li>
+     * <p>
+     * 
+     * @param resourceLocation
+     *            the resource location to resolve.
+     * 
+     * @return a corresponding {@link URL} object
+     * 
+     * @throws FileNotFoundException
+     *             if the resource cannot be resolved to a URL
+     */
+    public static URL getURL(String resourceLocation) throws FileNotFoundException {
+        if (resourceLocation.startsWith(URLProtocol.CLASSPATH.getProtocol())) {
+            return URLProtocol.CLASSPATH.toUrl(resourceLocation);
+        } else if (resourceLocation.startsWith(URLProtocol.JAVA.getProtocol())) {
+            return URLProtocol.JAVA.toUrl(resourceLocation);
+        } else if (resourceLocation.startsWith(URLProtocol.RESOURCE.getProtocol())) {
+            return URLProtocol.RESOURCE.toUrl(resourceLocation);
+        }
+
+        try {
+            return new URL(resourceLocation);
+        } catch (MalformedURLException ex) {
+            try {
+                return new File(resourceLocation).toURI().toURL();
+            } catch (MalformedURLException ex2) {
+                throw new FileNotFoundException("Resource location [" + resourceLocation + "] is neither a URL not a well-formed file path");
+            }
+        }
+    }
+
+    /**
+     * Constructs a new URL by parsing the given path fragment and then
+     * resolving it against the {@link URI}
+     * 
+     * @param baseUri
+     *            the base {@link URI} to resolve the given fragment against.
+     * @param fragment
+     *            the path fragment to be resolved.
+     * 
+     * @return a resolved {@link URL} object.
+     * 
+     * @throws FileNotFoundException
+     *             if the resource cannot be resolved to a URL
+     * 
+     * @see URLUtil#getURL which is used internally.
+     */
+    public static URL resolveURL(URI baseUri, String fragment) throws FileNotFoundException {
+        URI uri = baseUri.resolve(fragment);
+        return getURL(uri.toString());
     }
 }
